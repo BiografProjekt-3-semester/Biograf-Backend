d99629da284b7b5f9f835012c965a5d3054f6821
Martin Poulsen <mapo0004@stud.kea.dk>
2024-09-26 11:53:24 +0200

    test

---
+++ src/test/java/stud/kea/dk/biografbackend/BiografBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.biografbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiografBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
