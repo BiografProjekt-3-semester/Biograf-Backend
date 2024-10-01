b65b0cbbf2bb563e1a2888ab90acbcdff90221cd
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-10-01 14:49:58 +0200

    tesr

--- src/main/java/stud/kea/dk/biografbackend/test/test.java
+++ src/main/java/stud/kea/dk/biografbackend/test/test.java
@@ -1,8 +1,9 @@
 package stud.kea.dk.biografbackend.test;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class test {
 
