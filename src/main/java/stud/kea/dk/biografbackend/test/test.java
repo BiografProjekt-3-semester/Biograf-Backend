17fe2d3c1445095aa23671a07d962d5bbb2edcca
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-10-01 14:26:11 +0200

    dhdhd

--- src/main/java/stud/kea/dk/biografbackend/test/test.java
+++ src/main/java/stud/kea/dk/biografbackend/test/test.java
@@ -1,4 +1,15 @@
 package stud.kea.dk.biografbackend.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class test {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
 }
+
