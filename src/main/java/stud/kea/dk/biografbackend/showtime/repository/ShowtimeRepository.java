c9e265caf4ce5a50d1d232818cad9af935054e29
Viggo2102 <146332112+Viggo2102@users.noreply.github.com>
2024-10-03 11:02:13 +0200

    Application properties.

--- src/main/java/stud/kea/dk/biografbackend/showtime/repository/ShowtimeRepository.java
+++ src/main/java/stud/kea/dk/biografbackend/showtime/repository/ShowtimeRepository.java
@@ -1,7 +1,8 @@
 package stud.kea.dk.biografbackend.showtime.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import stud.kea.dk.biografbackend.showtime.model.ShowtimeModel;
 
-public interface ShowtimeRepository extends JpaRepository<ShowtimeRepository, Integer> {
+public interface ShowtimeRepository extends JpaRepository<ShowtimeModel, Integer> {
 
 }
