9ec8fd0ca235a8081454dc9ee5c22d2608638417
Oskar Eriksen <oska1001@stud.kea.dk>
2024-10-03 12:54:13 +0200

    CRUD-metoder til oprettelse af ny film - backend skulle gerne være færdig. Herunder service, repo implementeret.

--- src/main/java/stud/kea/dk/biografbackend/movie/service/ApiServiceGetMovieImpl.java
+++ src/main/java/stud/kea/dk/biografbackend/movie/service/ApiServiceGetMovieImpl.java
@@ -1,4 +1,40 @@
 package stud.kea.dk.biografbackend.movie.service;
 
-public class ApiServiceGetMovieImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import stud.kea.dk.biografbackend.movie.model.MovieModel;
+import stud.kea.dk.biografbackend.movie.repository.MovieRepository;
+
+import java.util.List;
+
+@Service
+public class ApiServiceGetMovieImpl implements ApiServiceGetMovie {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Override
+    public MovieModel createMovie(MovieModel movieModel) {
+        return movieRepository.save(movieModel);
+    }
+
+    @Override
+    public List<MovieModel> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @Override
+    public MovieModel getMovieById(int id) {
+        return movieRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public MovieModel updateMovie(MovieModel movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public void deleteMovie(int id) {
+        movieRepository.deleteById(id);
+    }
 }
