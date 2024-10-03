6a59c5f610d09a40afbdf4578923cb44c683aadd
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-10-03 10:07:59 +0200

    test data

---
+++ src/main/java/stud/kea/dk/biografbackend/contig/InititData.java
@@ -0,0 +1,31 @@
+package stud.kea.dk.biografbackend.contig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class InititData implements CommandLineRunner {
+
+    //@Autowired
+    //MovieRepository movieRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        /*
+        Movie movie1 = new Movie(1, "Inception", "A mind-bending thriller", 148, 13, "inception.jpg");
+        Movie movie2 = new Movie(2, "The Matrix", "A hacker discovers the real world", 136, 15, "matrix.jpg");
+        Movie movie3 = new Movie(3, "Frozen", "An animated tale of two sisters", 102, 7, "frozen.jpg");
+        Movie movie4 = new Movie(4, "Interstellar", "A team of explorers travel through a wormhole in space", 169, 13, "interstellar.jpg");
+        Movie movie5 = new Movie(5, "The Dark Knight", "Batman faces the Joker in Gotham City", 152, 15, "darkknight.jpg");
+        Movie movie6 = new Movie(6, "Avatar", "A paraplegic Marine on an alien planet", 162, 12, "avatar.jpg");
+        Movie movie7 = new Movie(7, "The Lion King", "A young lion prince flees his kingdom", 88, 6, "lionking.jpg");
+        Movie movie8 = new Movie(8, "Jurassic Park", "Dinosaurs run amok in a theme park", 127, 12, "jurassicpark.jpg");
+        Movie movie9 = new Movie(9, "Titanic", "A young couple falls in love aboard the ill-fated ship", 195, 12, "titanic.jpg");
+        Movie movie10 = new Movie(10, "Pulp Fiction", "The lives of two hitmen intertwine with other characters", 154, 18, "pulpfiction.jpg");
+        Movie movie11 = new Movie(11, "The Godfather", "The story of a powerful mafia family", 175, 16, "godfather.jpg");
+        Movie movie12 = new Movie(12, "Finding Nemo", "A clownfish searches for his missing son", 100, 6, "findingnemo.jpg");
+        Movie movie13 = new Movie(13, "Harry Potter and the Sorcerer's Stone", "A boy discovers he is a wizard", 152, 10, "harrypotter.jpg");
+            for (int i = 1; i <= 13; i++) {
+                movieRepository.save(i);
+            }
+         */
+    }
+}
