d63c4ac99aca5b79e4ce90f77b2012d2838f41b4
Martin Poulsen <mapo0004@stud.kea.dk>
2024-10-04 11:43:49 +0200

    rettet chairs så de ikke bliver laagt ind hele tiden
    Har rettet så når man køre programmer bliver stolene ikke oprettet hver gang hvis der ikker er nogle data i.

--- src/main/java/stud/kea/dk/biografbackend/chair/config/ChairInitData.java
+++ src/main/java/stud/kea/dk/biografbackend/chair/config/ChairInitData.java
@@ -14,36 +14,41 @@ import java.util.Optional;
 @Component
 @Order(2)
 public class ChairInitData implements CommandLineRunner {
-    @Autowired
-    ChairRepository chairRepository;
 
-    @Autowired
-    TheaterRepository theaterRepository;
+    final private ChairRepository chairRepository;
+    final private TheaterRepository theaterRepository;
+
+    public ChairInitData(ChairRepository chairRepository, TheaterRepository theaterRepository){
+        this.chairRepository = chairRepository;
+        this.theaterRepository = theaterRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        // Associating chairs
-        Optional<TheaterModel> theater1 = theaterRepository.findById(1);
-        Optional<TheaterModel> theater2 = theaterRepository.findById(2);
+        if (chairRepository.findAll().isEmpty()) {
+            // Associating chairs
+            Optional<TheaterModel> theater1 = theaterRepository.findById(1);
+            Optional<TheaterModel> theater2 = theaterRepository.findById(2);
 
-        if (theater1.isPresent()) {
-            // Inserting chairs into small hall one (20 rows, 12 seats per row)
-            for (int row = 1; row <= 20; row++) {
-                for (int seat = 1; seat <= 12; seat++) {
-                    boolean isSpecialUse = (row == 20 && seat >= 8); // Last 5 chairs in the last row
-                    ChairModel chair = new ChairModel(0, seat, row, true, isSpecialUse, theater1.get());
-                    chairRepository.save(chair);
+            if (theater1.isPresent()) {
+                // Inserting chairs into small hall one (20 rows, 12 seats per row)
+                for (int row = 1; row <= 20; row++) {
+                    for (int seat = 1; seat <= 12; seat++) {
+                        boolean isSpecialUse = (row == 20 && seat >= 8); // Last 5 chairs in the last row
+                        ChairModel chair = new ChairModel(0, seat, row, true, isSpecialUse, theater1.get());
+                        chairRepository.save(chair);
+                    }
                 }
             }
-        }
 
-        if (theater2.isPresent()) {
-            // Inserting chairs into large hall two (25 rows, 16 seats per row)
-            for (int row = 1; row <= 25; row++) {
-                for (int seat = 1; seat <= 16; seat++) {
-                    boolean isSpecialUse = (row == 25 && seat >= 12); // Last 5 chairs in the last row
-                    ChairModel chair = new ChairModel(0, seat, row, true, isSpecialUse, theater2.get());
-                    chairRepository.save(chair);
+            if (theater2.isPresent()) {
+                // Inserting chairs into large hall two (25 rows, 16 seats per row)
+                for (int row = 1; row <= 25; row++) {
+                    for (int seat = 1; seat <= 16; seat++) {
+                        boolean isSpecialUse = (row == 25 && seat >= 12); // Last 5 chairs in the last row
+                        ChairModel chair = new ChairModel(0, seat, row, true, isSpecialUse, theater2.get());
+                        chairRepository.save(chair);
+                    }
                 }
             }
         }
