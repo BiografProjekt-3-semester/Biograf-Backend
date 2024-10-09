cf5a35188252decd68d16309d9d54ed6fb2ff648
Martin Poulsen <mapo0004@stud.kea.dk>
2024-10-09 09:18:25 +0200

    rettelse så den ikke laver showtimes 30min efter hindnden
    rettelse så den ikke laver showtimes 30min efter hindanden

--- src/main/java/stud/kea/dk/biografbackend/showtime/config/ShowTimeInitData.java
+++ src/main/java/stud/kea/dk/biografbackend/showtime/config/ShowTimeInitData.java
@@ -36,36 +36,60 @@ public class ShowTimeInitData implements CommandLineRunner {
             List<TheaterModel> theaters = theaterRepository.findAll();
 
             if (!movies.isEmpty() && !theaters.isEmpty()) {
-                // Varighed for hver af de 10 film
-                //koden skal rettes til hvis der er flere film
+                // Varigheden på hver film
                 int[] movieDurations = {100, 87, 88, 92, 91, 83, 104, 84, 83, 89};
                 Random random = new Random();
                 LocalDate today = LocalDate.now();
 
-                // Laver x showtimes for hver film (skal rette i j < x)
+                // Laver to showtimes for hver film
                 for (int i = 0; i < 10; i++) {
                     MovieModel movie = movies.get(i);
                     int duration = movieDurations[i];
 
-                    for (int j = 0; j < 3; j++) {
-                        // Vælger tilfældig sal
-                        TheaterModel randomTheater = theaters.get(random.nextInt(theaters.size()));
-                        // Vælger tilfældig tid mellem 10:00 og 20:59
-                        LocalTime randomStartTime = LocalTime.of(random.nextInt(10) + 10, random.nextInt(60));
-                        LocalTime endTime = randomStartTime.plusMinutes(duration);
+                    // Laver den første showtime
+                    TheaterModel randomTheater1 = theaters.get(random.nextInt(theaters.size()));
+                    // Laver tilfældig tid mellem 10:00 og 20:59
+                    LocalTime randomStartTime1 = LocalTime.of(random.nextInt(10) + 10, random.nextInt(60));
+                    LocalTime endTime1 = randomStartTime1.plusMinutes(duration);
 
-                        ShowtimeModel showtime = new ShowtimeModel();
-                        // Vælger tilfældig dato inden for 30 dage
-                        showtime.setMovieDate(today.plusDays(random.nextInt(30)));
-                        showtime.setStartTime(randomStartTime);
-                        showtime.setEndTime(endTime);
-                        // Vælger tilfældig pris mellem 75 og 100
-                        showtime.setPrice(75.0 + random.nextDouble() * 25.0);
-                        showtime.setMovie(movie);
-                        showtime.setTheater(randomTheater);
-                        // Gemmer hver showtime
-                        showtimeRepository.save(showtime);
-                    }
+                    ShowtimeModel showtime1 = new ShowtimeModel();
+                    // Laver tilfældig dato indien for 30 dage
+                    showtime1.setMovieDate(today.plusDays(random.nextInt(30)));
+                    showtime1.setStartTime(randomStartTime1);
+                    showtime1.setEndTime(endTime1);
+                    // Laver tilfældig pris mellem 75 og 100
+                    showtime1.setPrice(75.0 + random.nextDouble() * 25.0);
+                    showtime1.setMovie(movie);
+                    showtime1.setTheater(randomTheater1);
+
+                    // Gemmer den først showtime
+                    showtimeRepository.save(showtime1);
+
+                    // Sikre den anden showtime er mindst 30 min efter slutningen af den første
+                    LocalTime randomStartTime2;
+                    do {
+                        // Laver tilfældig tid mellem 10:00 og 20:59
+                        randomStartTime2 = LocalTime.of(random.nextInt(10) + 10, random.nextInt(60));
+                        // Sikre der er mindst 30 min i mellem
+                    } while (Math.abs(randomStartTime2.toSecondOfDay() - endTime1.toSecondOfDay()) < 1800);
+
+                    LocalTime endTime2 = randomStartTime2.plusMinutes(duration);
+
+                    // Laver den anden showtime
+                    // Laver tilfældig showtime for den anden showtime
+                    TheaterModel randomTheater2 = theaters.get(random.nextInt(theaters.size()));
+                    ShowtimeModel showtime2 = new ShowtimeModel();
+                    // Laver tilfældig dato inden for 30 dage
+                    showtime2.setMovieDate(today.plusDays(random.nextInt(30)));
+                    showtime2.setStartTime(randomStartTime2);
+                    showtime2.setEndTime(endTime2);
+                    // Laver tilfældig pris mellem 75 og 100
+                    showtime2.setPrice(75.0 + random.nextDouble() * 25.0);
+                    showtime2.setMovie(movie);
+                    showtime2.setTheater(randomTheater2);
+
+                    // Gemmer den anden showtime
+                    showtimeRepository.save(showtime2);
                 }
             }
         }
